d34d16cecc77a36c69e12059ebfd9c0dd2711fe6
yuansun235 <yuansun235@163.com>
2019-01-27 20:43:45 +0800

    Project for building

--- src/router.js
+++ src/router.js
@@ -18,10 +18,11 @@ export default new Router({
     {
       path: '/mission',
       name: 'mission',
+      component: Mission
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Mission.vue')
+      // component: () => import(/* webpackChunkName: "about" */ './views/Mission.vue')
     },
     {
       path: '/story',
