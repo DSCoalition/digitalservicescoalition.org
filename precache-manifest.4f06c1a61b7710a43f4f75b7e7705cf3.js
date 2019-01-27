f37eae74a148ab6a2424c532596764ed2d3d9a15
yuansun235 <yuansun235@163.com>
2019-01-28 00:22:17 +0800

    Deployment Files

---
+++ precache-manifest.4f06c1a61b7710a43f4f75b7e7705cf3.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb63126a1fba1cbc5acf551b8d1c6547",
+    "url": "/digitalservicescoalition.org/img/bg-rectangle-blue.fb63126a.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/digitalservicescoalition.org/robots.txt"
+  },
+  {
+    "revision": "e2b249c6098eadcd407c0d5118c0638d",
+    "url": "/digitalservicescoalition.org/img/logo-fearless.e2b249c6.png"
+  },
+  {
+    "revision": "84e0ab94dfb04f978415",
+    "url": "/digitalservicescoalition.org/css/chunk-vendors.2da46af1.css"
+  },
+  {
+    "revision": "7dc3043a2b3c800b7a60a03f73cde5ce",
+    "url": "/digitalservicescoalition.org/img/logo-stsi.7dc3043a.png"
+  },
+  {
+    "revision": "0182e7b663f39cc445b7768a4b373e7c",
+    "url": "/digitalservicescoalition.org/img/logo-mediabarn.0182e7b6.png"
+  },
+  {
+    "revision": "b94c836dd9a22f807dcff8d08e2b526c",
+    "url": "/digitalservicescoalition.org/img/bg-triangle-pink2.b94c836d.png"
+  },
+  {
+    "revision": "b1a0097268463b6075cb02db777b1c8b",
+    "url": "/digitalservicescoalition.org/img/logo-mo.b1a00972.png"
+  },
+  {
+    "revision": "71f2c83ab54ebda5c1591143f71565f9",
+    "url": "/digitalservicescoalition.org/img/logo-adhoc.71f2c83a.png"
+  },
+  {
+    "revision": "002ec039b69779b9017db8d77aabe340",
+    "url": "/digitalservicescoalition.org/img/logo-agilesix.002ec039.png"
+  },
+  {
+    "revision": "45b1e933dc625c72fe652b3fcec901bf",
+    "url": "/digitalservicescoalition.org/img/bg-triangle-pink.45b1e933.png"
+  },
+  {
+    "revision": "438f806f783d21f35ce4f0fe08417346",
+    "url": "/digitalservicescoalition.org/img/bg-triangle-blue.438f806f.png"
+  },
+  {
+    "revision": "683696628d73144d2ab5027b1d5e5c43",
+    "url": "/digitalservicescoalition.org/img/logo-skylight.68369662.png"
+  },
+  {
+    "revision": "31196a3f036660f04426599fa1b79a66",
+    "url": "/digitalservicescoalition.org/img/logo-nava.31196a3f.png"
+  },
+  {
+    "revision": "931510cf48816bae83eb",
+    "url": "/digitalservicescoalition.org/js/app.9eb4e620.js"
+  },
+  {
+    "revision": "84e0ab94dfb04f978415",
+    "url": "/digitalservicescoalition.org/js/chunk-vendors.c1889d8c.js"
+  },
+  {
+    "revision": "e3d43157f859db9368bc27d11420142a",
+    "url": "/digitalservicescoalition.org/img/logo-andpartners.e3d43157.png"
+  },
+  {
+    "revision": "8910e24a5bcb0190311f7c08daed854d",
+    "url": "/digitalservicescoalition.org/img/bg-triangle-blue3.8910e24a.png"
+  },
+  {
+    "revision": "5a6f946fe43d4acfcc670505dfab3fff",
+    "url": "/digitalservicescoalition.org/img/logo-civicactions.5a6f946f.png"
+  },
+  {
+    "revision": "8a33b0d8d5f492ed3c96a7f2c249d671",
+    "url": "/digitalservicescoalition.org/img/logo-exygy.8a33b0d8.png"
+  },
+  {
+    "revision": "d021d0359b428c69c0d466bac374c2a1",
+    "url": "/digitalservicescoalition.org/img/illustration-gov.d021d035.png"
+  },
+  {
+    "revision": "266d02c67d11e0b520b56d428181186b",
+    "url": "/digitalservicescoalition.org/img/logo-thesocompany.266d02c6.png"
+  },
+  {
+    "revision": "1c937b7fbd3864e0373e8ad9e7810960",
+    "url": "/digitalservicescoalition.org/img/logo-oddball.1c937b7f.png"
+  },
+  {
+    "revision": "793c682ef2809295387d513233e0cc1c",
+    "url": "/digitalservicescoalition.org/img/dsc-logo.793c682e.png"
+  },
+  {
+    "revision": "1f54e43a5a239ef3a7e132e3265623ca",
+    "url": "/digitalservicescoalition.org/img/bg-triangle-gray.1f54e43a.png"
+  },
+  {
+    "revision": "84a38b7a17096e5f24a6e374dd14dfe1",
+    "url": "/digitalservicescoalition.org/index.html"
+  },
+  {
+    "revision": "d021d0359b428c69c0d466bac374c2a1",
+    "url": "/digitalservicescoalition.org/img/illustration-gov.png"
+  },
+  {
+    "revision": "72ce9230ec618bfe26f0b38f3469bdf3",
+    "url": "/digitalservicescoalition.org/favicon.png"
+  },
+  {
+    "revision": "931510cf48816bae83eb",
+    "url": "/digitalservicescoalition.org/css/app.21faf295.css"
+  }
+];
