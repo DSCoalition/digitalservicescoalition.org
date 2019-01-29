7886c39c1beefb7758709e35e5acffdc9fdf1542
bhalt <bhalteman@fearsol.com>
2019-01-29 17:48:55 -0500

    add additional bg image

--- src/views/Story.vue
+++ src/views/Story.vue
@@ -17,6 +17,12 @@
             </div>
         </div>
 
+        <div class="bg-parallax">
+            <parallax :speed-factor="0.3" sectionClass="parallax-outside" containerClass="parallax-inside">
+                <img src="@/assets/bg-triangle-gray.png" />
+            </parallax>
+        </div>
+        
         <div class="container py-5">
             <div class="row">
                 <div class="col-md-8">
