ec529c63d16b326c9dad79fc65b09427fba6202b
yuansun235 <yuansun235@163.com>
2019-03-14 00:09:11 +0800

    Embed google form for contact form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -15,31 +15,8 @@
 
                 <div class="col-md-5 py-md-5">
                     <h5 class="text-gray">Have a question about what we’re up to? Want to know more about working with us? Drop us a note!</h5>
-                    
-                    <div v-if="isSent" id="contact-form-success" class="bg-gray">
-                        <p>Your message was sent! Thanks for reaching out.</p>
-                    </div>
 
-                    <form v-if="!isSent" id="contact-form"  method="post" @submit.prevent="validateForm">
-                        <div class="form-group">
-                            <label for="first_name" :class="{'text-red': errors.has('first_name') }">First name <span class="text-red">*</span> <small v-show="errors.has('first_name')" class="text-gray">{{ errors.first('first_name') }}</small></label>
-                            <input v-validate="'required'" :class="{'form-control': true, 'border-red': errors.has('first_name') }" id="first_name" maxlength="40" name="first_name" size="20" type="text" v-model="form.first_name" />
-                        </div>
-                        <div class="form-group">
-                            <label for="last_name" :class="{'text-red': errors.has('last_name') }">Last name <span class="text-red">*</span> <small v-show="errors.has('last_name')" class="text-gray">{{ errors.first('last_name') }}</small></label>
-                            <input v-validate="'required'" :class="{'form-control': true, 'border-red': errors.has('last_name') }" id="last_name" maxlength="80" name="last_name" size="20" type="text" v-model="form.last_name" />
-                        </div>
-                        <div class="form-group">
-                            <label for="email" :class="{'text-red': errors.has('email') }">Email <span class="text-red">*</span> <small v-show="errors.has('email')" class="text-gray">{{ errors.first('email') }}</small></label>
-                            <input v-validate="'required|email'" data-vv-delay="500" :class="{'form-control': true, 'border-red': errors.has('email') }" id="email" maxlength="80" name="email" size="20" type="text" v-model="form.email" />
-                        </div>
-                        <div class="form-group">
-                            <label for="message" :class="{'text-red': errors.has('message') }">What's on your mind? <span class="text-red">*</span> <small v-show="errors.has('message')" class="text-gray">{{ errors.first('message') }}</small></label>
-                            <textarea v-validate="'required'" :class="{'form-control': true, 'border-red': errors.has('message') }" id="message" name="message" rows="5" v-model="form.message"></textarea>
-                        </div>
-                        <input v-if="!isSending" class="btn btn-primary btn-block" type="submit" name="submit_btn" value="Send" />
-                        <div v-if="isSending" class="btn btn-primary btn-block disabled">Sending</div>
-                    </form>
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfOo7ss8JtJ1mWCNdXoUNB3F-pj6OSabhmt9ZBeE6PcVQ1dDA/viewform?embedded=true" width="100%" height="1020" frameborder="0" marginheight="0" marginwidth="0"></iframe>
                 </div>
             </div>
         </div>
