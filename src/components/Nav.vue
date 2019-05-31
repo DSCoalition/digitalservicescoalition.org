babc0ad97e16ac446783e2bcd06c47fba44ef4d5
bhalt <bhalteman@fearsol.com>
2019-05-31 07:50:54 -0400

    add medium blog link to nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -28,6 +28,9 @@
                     <router-link to="/membership" class="nav-link">Membership</router-link>
                 </li>
                 <li class="nav-item">
+                    <a href="https://medium.com/@DigitalServicesCoalition" class="nav-link" target="_blank">News</a>
+                </li>
+                <li class="nav-item">
                     <a href="https://shop.digitalservicescoalition.org/" class="nav-link" target="_blank">Shop</a>
                 </li>
                 <li class="nav-item">
