6d3146ec8855cd3d9cff80a65f396be8288c8a4b
Daniel Miller <54035677+DanielMiller-agile6@users.noreply.github.com>
2021-05-13 14:12:26 -0700

    npm audit fix and lintfix (#48)

--- src/main.js
+++ src/main.js
@@ -1,27 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './registerServiceWorker'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '@/main.less'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./registerServiceWorker";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@/main.less";
 
-Vue.config.productionTip = false
-
-
+Vue.config.productionTip = false;
 
 // Form Validation
-import VeeValidate from 'vee-validate'
-Vue.use(VeeValidate)
-
-// Parallax Scrolling
-import Parallax from 'vue-parallaxy'
-
-
+import VeeValidate from "vee-validate";
+Vue.use(VeeValidate);
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
-
-
+  render: (h) => h(App),
+}).$mount("#app");
