8fefe37d3df6d7c9573edff2df5a83409125633f
Daniel Miller <daniel.miller@agile6.com>
2021-06-28 10:36:18 -0700

    update home page copy to reflect growing number of dsc companies

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,7 @@
                 <div class="col-md-6 py-5">
                     <h1 class="text-blue my-md-5">Redefining how the government does digital.</h1>
                     <h3 class="text-gray">We’re a collection of agile technology and design firms that serve—or aspire to serve—the government.</h3>
-                    <p>Change is hard. Systematic change is even harder. That’s why 16 design and development firms have teamed up to bring the innovation and agility that’s become the backbone of commercial digital services to the government.</p>
+                    <p>Change is hard. Systematic change is even harder. That’s why these design and development firms have teamed up to bring the innovation and agility that’s become the backbone of commercial digital services to the government.</p>
                     <p>This kind of digital transformation uses technology as a means to a goal—to dramatically improve the performance, reach, and customer experience of government services.</p>
                 </div>
             </div>
