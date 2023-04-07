b1debbd0d6fd0659008262ad3baedde3ecd66e4f
SeaJerry <jsea@kindsys.us>
2023-04-07 14:55:42 -0700

    Update membership view content and pdfs

--- src/views/Membership.vue
+++ src/views/Membership.vue
@@ -9,29 +9,84 @@
             <div class="row justify-content-md-between">
                 <div class="col-md-6 py-md-5">
                     <h1 class="text-magenta my-md-5">Join the coalition</h1>
-
                     <div class="alert alert-success" role="alert">
                         <h2 class="h4"><b>Announcement</b></h2>
-                        <b>We're not accepting new member applications at this time. <br> If you want to get on our mailing list, contact us at <a href="mailto: membership@digitalservicescoalition.org">membership@digitalservicescoalition.org</a></b>
+                        <b>We are now accepting applications for membership in the DSC from April 10th through May 7th, 2023.<br/><br/>Prior to filling out an application please review the 2023 DSC Membership Policy to ensure your company is eligible and to see the cost of membership: </b><b><a href="/pdfs/Membership-Policy-2023.pdf" target="_blank">DSC 2023 Membership Policy</a></b><br/><br/>
+                        <b>Fill out your application here: <a href="https://www.surveymonkey.com/r/BLNLCZJ">DSC 2023 Membership Application</a></b>
                     </div>
-                    <h2 class="text-gray mt-5">Membership benefits</h2>
-                    <ul>
-                        <li>Access to DSC network</li>
-                        <li>Internal teaming</li>
-                        <li>Large businesses = access to database of small DSC firms for teaming</li>
-                        <li>Inclusion in a list of “approved vendors” for Government and Large Business</li>
-                        <li>Inherit engines of growth to leverage inherent motivation</li>
-                        <li>Marketing/Press around wins</li>
-                        <li>Ability to bring up issues for advocacy by the DSC</li>
-                        <li>GovCon education</li>
-                    </ul>
-                    <h2 class="text-gray mt-5">Who can join</h2>
-                    <p>We have a phased approach to membership, starting with small-sized or mid-tier businesses. Large firms may be considered in the future.</p>
-                    <ul>
-                        <li>Small-sized (revenue less than $27.5M for a 3 year running average) or mid-tier (revenue less than $55M for a 3 year running average) businesses who perform Digital Services work. Digital Services work is defined as a service that is delivered via the internet or electronic network, and automated to require little to no human intervention.</li>
-                        <li>Small-sized or mid-tier businesses who provide services for the public sector or have a desire to do work in the public sector.</li>
-                        <li>Organizations outside of digital services and the public sector that align with DSC values. What alternative services qualify? You tell us! Articulate how your service fits with our mission and outline a pitch of how you would like to get involved.</li>
-                    </ul>
+                    <div class="who-can-join">
+                        <h2 class="text-gray mt-5">Who can join</h2>
+                        <p class="list-items">Small-sized or mid-tier businesses who perform digital service or digital transformation work for the public sector or have a desire to do work in the public sector. Large firms may be considered at a future date to be determined.</p>
+                        <ol type="i">
+                            <li>Small businesses: revenue less than $30M ( 3 year running average)</li>
+                            <li>Mid-tier firms: revenue less than $55M (3 year running average)</li>
+                            <li>Large firms: revenue greater than $55M (3 year running average)</li>
+                            <li>Confirmation of size standard will be conducted in on-boarding activities. Firms must provide proof in the form of: tax returns, tax audits, small business size standard certification from SBA, or other means. This information will be kept confidential.</li>
+                        </ol>
+                        <p class="list-items">Digital service work is defined as services supporting the development, implementation, or maintenance of:</p>
+                        <ol type="i">
+                            <li>A service that is delivered via the internet or electronic network</li>
+                            <li>Automated and requires little to no human intervention</li>
+                        </ol>
+                        <p class="list-items">Digital transformations are efforts that have fundamentally improved the technologies, data, processes, and organizational change necessary to deliver successful outcomes for users and customers</p>
+                        <p class="list-items">Academic institutions that align with the values and mission of the DSC.</p>
+                        <p class="list-items">Firms who are committed to following the DSC’s documented Community Standards and Organizational Bylaws.</p>
+                    </div>
+                    <div class="membership-benefits">
+                        <h2 class="text-gray mt-5">Membership benefits</h2>
+                        <p>This organization is committed to the growth and development of the digital service industry. Members in the DSC have access to business resources, mentorship from companies and individuals with similar experiences, exclusive and impactful networking, education, and advocacy opportunities, and business development resources.</p>
+                        <div>
+                            <b>Education:</b>
+                            <p>DSC offers varied education opportunities including: </p>
+                            <ul>
+                                <li>Webinars and workshops based on topics of interest to both the government and industry with ability to earn continuous education units (CEUs) which support professional development & certification.</li>
+                                <li>Brown Bag discussions and Ask Me Anything with subject matter experts to provide collaborative experiences that are shared among multiple companies</li>
+                                <li>Hands - on or strategic training sessions one - on - one with member companies</li>
+                                <li>Opportunities to speak on behalf of the DSC and its community at trade shows, meet-ups, and other external events</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <b>Events:</b>
+                            <p>Networking is the lifeblood of the digital service community and the DSC provides many events - both in-person and online to connect and reinforce relationships, including:</p>
+                            <ul>
+                                <li>Yearly all hands meetings for overall DSC updates</li>
+                                <li>DSC member happy hours and networking events </li>
+                                <li>Networking and education events with government partners</li>
+                                <li>Fun events- such as DSC hikes, picnics, and holiday parties</li>
+                                <li>Virtual happy hours and events to ensure the distributed members have the ability to connect and network.</li>
+                                <li>Soon to come: DSC Summits & larger events</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <b>Advocacy & Policy:</b>
+                            <p>The DSC represents the members interest in government legislation, policy, and regulations that affect our industry: </p>
+                            <ul>
+                                <li>Participates in legislative reviews on upcoming or revision of proposed legislation such as the Clear & Concise Writing Act, FITARA, NDAA, etc. </li>
+                                <li>Assists government in understanding and applying executive orders such as the Customer Experience Executive Orders and the Cybersecurity Executive Orders. </li>
+                                <li>Advocates on behalf of and to the government in areas such as accessibility, diversity, equity, and inclusion.</li>
+                                <li>Conducts activities to improve opportunities for socio-economic categories and under-represented talent in the tech industry.</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <b>Market Intelligence and Business Development:</b>
+                            <p>Conducts activities to understand and grow the industry opportunities for digital service firms: </p>
+                            <ul>
+                                <li>Collaborates with similar non-profit organizations and trade associations to grow the community and ecosystem as a means to build and maintain influence for digital service work</li>
+                                <li>Reviews RFI’s, RFQs, and other government opportunities to provide feedback to government on best practices, better evaluation strategies, or to protect company interests when making decisions on future work.</li>
+                                <li>Provides strategy or advice on large government contract actions such as GSA Schedules, GWACs, agency contracts, etc.</li>
+                                <li>Networks with potential large or other partners in other industries to keep up a partnering network for member companies available.</li>
+                                <li>Socioeconomic categories are well represented and special programming exists for each specific category to support set asides and marketing.</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <b>Member Saving Program:</b>
+                            <p>Through business relationships with other groups and organizations</p>
+                            <ul>
+                                <li>Gets group discounts for DSC member companies, such as for G2Exchange subscriptions, Rosenfeld CivicDesign Conferences, and others.</li>
+                            </ul>
+                            <p>Builds in discounts or no charges for any DSC led activity.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -50,13 +105,12 @@
                     <div class="col-md-6 py-5">
                         <h2 class="text-gray mt-5">Criteria for membership</h2>
                         <ul>
-                            <li>Proof of Digital Services Work/Work Sample (at least two)</li>
-                            <li>Must actively support two initiatives as a member firm that map into the current DSC yearly priorities</li>
-                            <li>Two Customer references</li>
-                            <li>Commitment to attendance at regular meetings with DSC members</li>
-                            <li>Sponsorship application submitted to DSC from current member</li>
+                            <li>Completed application, including two digital services case studies where work was performed within the last three years, with customer references for each. <a href="/pdfs/Appendix-A-Case-Study-Instructions-2023.pdf" target="_blank">2023 Case Study Instructions </a></li>
+                            <li>Must actively support two initiatives as a member firm that map into the current DSC yearly priorities. </li>
+                            <li>Sponsorship from a current DSC member.</li>
+                            <li>Commitment to attendance at regular meetings with DSC members.</li>
                         </ul>
-                        <p>For more information on the full membership criteria and qualifications, please review <a href="https://tinyurl.com/4vrdzzr4 ">2022 DSC Membership Policy</a>.</p>
+                        <p>For more information on the full membership criteria and qualifications, please review <a href="/pdfs/Membership-Policy-2023.pdf" target="_blank">2023 DSC Membership Policy</a>.</p>
                     </div>
                 </div>
             </div>
